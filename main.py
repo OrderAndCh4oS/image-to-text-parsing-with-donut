90aa2705e7e5802b9d8feebfab6d1304d6b322f2
order&chaos <sean@orderandchaoscreative.com>
2023-05-05 21:20:21 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,51 @@
+from PIL import Image
+
+import re
+
+from transformers import DonutProcessor, VisionEncoderDecoderModel
+from datasets import load_dataset
+import torch
+
+processor = DonutProcessor.from_pretrained("naver-clova-ix/donut-base-finetuned-cord-v2")
+model = VisionEncoderDecoderModel.from_pretrained("naver-clova-ix/donut-base-finetuned-cord-v2")
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model.to(device)
+# dataset = load_dataset("hf-internal-testing/example-documents", split="test")
+# src_image = dataset[2]["image"]
+# print(src_image)
+src_image = Image.open('./img_one.png')
+src_image = src_image.convert('RGB')
+print(src_image)
+
+task_prompt = "<s_cord-v2>"
+# task_prompt = "<s_rvlcdip>"
+# task_prompt = "<s_docvqa><s_question>{user_input}</s_question><s_answer>"
+decoder_input_ids = processor.tokenizer(task_prompt, add_special_tokens=False, return_tensors="pt").input_ids
+
+
+def main(image):
+    pixel_values = processor(image, return_tensors="pt").pixel_values
+
+    outputs = model.generate(
+        pixel_values.to(device),
+        decoder_input_ids=decoder_input_ids.to(device),
+        max_length=model.decoder.config.max_position_embeddings,
+        early_stopping=True,
+        pad_token_id=processor.tokenizer.pad_token_id,
+        eos_token_id=processor.tokenizer.eos_token_id,
+        use_cache=True,
+        num_beams=1,
+        bad_words_ids=[[processor.tokenizer.unk_token_id]],
+        return_dict_in_generate=True,
+    )
+
+    sequence = processor.batch_decode(outputs.sequences)[0]
+    sequence = sequence.replace(processor.tokenizer.eos_token, "").replace(processor.tokenizer.pad_token, "")
+    sequence = re.sub(r"<.*?>", "", sequence, count=1).strip()  # remove first task start token
+    print(processor.token2json(sequence))
+
+
+if __name__ == '__main__':
+    main(src_image)
+    print('DONE')
